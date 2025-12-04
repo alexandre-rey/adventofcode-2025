90db5f79bd7c114f8619a19ad0d313b275566c36
alexandre.rey <alexandre.rey74@outlook.com>
2025-12-04 22:39:12 +0100

    Day 3 part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
+use crate::part_1::Part1;
+
+mod part_1;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input.txt").unwrap();
+
+    let part_1 = Part1::new(&input);
+    part_1.run();
 }
