c28945cd132f6f3a87ad8428cb444522793755c0
alexandre.rey <alexandre.rey74@outlook.com>
2025-12-04 22:48:16 +0100

    Day 3 part 1

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs;
 
-use crate::part_1::Part1;
+use crate::{part_1::Part1, part_2::Part2};
 
 mod part_1;
+mod part_2;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
 
     let part_1 = Part1::new(&input);
     part_1.run();
+
+    let part_2 = Part2::new(&input);
+    part_2.run();
 }
