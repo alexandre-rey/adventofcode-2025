9bec9d1e7a9ba4a9c87f5b2d469d69f541f6c0e1
alexandre.rey <alexandre.rey74@outlook.com>
2025-12-05 00:19:35 +0100

    Part 2

--- day-3/src/part_2.rs
+++ day-3/src/part_2.rs
@@ -10,26 +10,11 @@ impl Part2 {
     }
 
     pub fn run(&self) {
-        let mut max_per_line: Vec<u32> = Vec::new();
+        let mut all_values: Vec<String> = Vec::new();
 
         for line in self.input.lines() {
             let mut cm = 0;
-            for i in 0..line.len() {
-                let ci = line.get(i..(i + 1)).unwrap();
-                for j in (i + 1)..line.len() {
-                    let cj = line.get(j..(j + 1)).unwrap();
-                    let mut tmp = ci.to_string();
-                    tmp.push_str(cj);
-                    let tmp: u32 = tmp.parse().unwrap();
-                    if tmp > cm {
-                        cm = tmp;
-                    }
-                }
-            }
-            println!("line cm: {cm}");
-            max_per_line.push(cm);
+            for i in 0..line.len() {}
         }
-        let sum: u32 = max_per_line.iter().sum();
-        println!("Max joltage: {}", sum);
     }
 }
